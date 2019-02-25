08939704f01335ed1493b17a4e52ef437e809ff4
JYX <jyx17@mails.tsinghua.edu.cn>
2019-02-25 19:29:40 +0800

    Fix inline formula bug
    Fix issue #1. 
    `state.posMax` means the total string length, not the max index in the string. (weird)
    The previous implementation led to NaN comparison.

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -2,8 +2,12 @@
 
 function isCloseable(state, pos) {
   const prevChar = pos > 0 ? state.src.charCodeAt(pos - 1) : -1
-  const nextChar = pos + 1 <= state.posMax ? state.src.charCodeAt(pos + 1) : -1
-
+  
+  // Fix issue #1. 
+  // state.posMax means the total string length, not the max index in the string.(weird)
+  // The previous implementation led to NaN comparison.
+  const nextChar = pos + 1 < state.posMax ? state.src.charCodeAt(pos + 1) : -1
+  
   return (
     prevChar !== 0x20 /* " " */ &&
     prevChar !== 0x09 /* "\t" */ &&
