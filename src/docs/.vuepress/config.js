341428bd000676969d92d9faffece4741f7f4b5b
Shigma <1700011071@pku.edu.cn>
2019-02-26 20:33:51 +0800

    update config

--- src/docs/.vuepress/config.js
+++ src/docs/.vuepress/config.js
@@ -1,79 +1,16 @@
-const path = require('path')
+const { getBaseConfig } = require('@shigma/vuepress-utils')
 
-function resolve (...args) {
-  return path.resolve(__dirname, '..', ...args)
-}
-
-const guideSidebar = () => [
-  '',
-  'config.html',
-  'changelog.html',
-]
-
-module.exports = {
-  base: '/vuepress-plugin-mathjax/',
-  dest: 'docs',
-
-  plugins: [
-    '@vuepress/medium-zoom',
-    '@vuepress/back-to-top',
-    ['@vuepress/nprogress', false],
-    ['redirect', {
-      locales: {
-        storage: true,
-      },
-    }],
-    ['mathjax', {
+module.exports = getBaseConfig({
+  name: 'vuepress-plugin-mathjax',
+  locales: {
+    en: 'A VuePress plugin which supports TeX syntax',
+    zh: '一个提供 TeX 语法支持的 VuePress 插件',
+  },
+  plugins: {
+    mathjax: {
       macros: {
         '\\Z': '\\mathbb{Z}'
       }
-    }],
-  ],
-
-  locales: {
-    '/en/': {
-      lang: 'en-US',
-      title: 'vuepress-plugin-mathjax',
-      description: 'A VuePress plugin which supports TeX syntax',
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'vuepress-plugin-mathjax',
-      description: '一个提供 TeX 语法支持的 VuePress 插件',
     },
   },
-  
-  themeConfig: {
-    repo: 'Shigma/vuepress-plugin-mathjax',
-    editLinks: true,
-    docsDir: 'src/docs',
-    locales: {
-      '/en/': {
-        label: 'English',
-        selectText: 'Languages',
-        editLinkText: 'Edit this page on GitHub',
-        lastUpdated: 'Last Updated',
-        nav: [
-          { text: 'Home', link: '/en/' },
-          { text: 'Guide', link: '/en/guide/' },
-        ],
-        sidebar: {
-          '/en/guide/': guideSidebar(),
-        },
-      },
-      '/zh/': {
-        label: '简体中文',
-        selectText: '选择语言',
-        editLinkText: '在 GitHub 上编辑此页',
-        lastUpdated: '上次更新',
-        nav: [
-          { text: '主页', link: '/zh/' },
-          { text: '指南', link: '/zh/guide/' },
-        ],
-        sidebar: {
-          '/zh/guide/': guideSidebar(),
-        },
-      },
-    },
-  },
-}
+})
