1088a056771565fad75b1d72a2ab116ff5e9317f
Shigma <1700011071@pku.edu.cn>
2019-03-01 10:38:36 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,70 @@
-const { getBaseConfig } = require('@shigma/vuepress-utils')
+const name = 'vuepress-plugin-mathjax'
 
-module.exports = getBaseConfig({
-  name: 'vuepress-plugin-mathjax',
-  locales: {
-    en: 'A VuePress plugin which supports TeX syntax',
-    zh: '一个提供 TeX 语法支持的 VuePress 插件',
-  },
-  plugins: {
-    mathjax: {
+const guideSidebar = () => [
+  '',
+  'config.html',
+  'changelog.html',
+]
+
+module.exports = ({ isProd }) => ({
+  base: `/${name}/`,
+
+  plugins: [
+    '@vuepress/medium-zoom',
+    '@vuepress/back-to-top',
+    ['mathjax', {
       macros: {
         '\\Z': '\\mathbb{Z}'
       }
+    }],
+  ],
+  
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: name,
+      description: 'A VuePress plugin which supports TeX syntax',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: name,
+      description: '一个提供 TeX 语法支持的 VuePress 插件',
     },
   },
+  
+  themeConfig: {
+    repo: `Shigma/${name}`,
+    editLinks: true,
+    docsDir: 'src/docs',
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Languages',
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated',
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+        ],
+        sidebar: {
+          '/guide/': guideSidebar(),
+        },
+      },
+      '/zh/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          { text: '主页', link: '/zh/' },
+          { text: '指南', link: '/zh/guide/' },
+        ],
+        sidebar: {
+          '/zh/guide/': guideSidebar(),
+        },
+      },
+    },
+  },
+
+  evergreen: !isProd,
 })
